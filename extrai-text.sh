c0663ce3c9974075e34a9fca099e1214436162aa
Marcio Junior Vieira <marcio@ambientelivre.com.br>
2018-05-23 22:18:24 -0300

    gerado todos TXTs

--- extrai-text.sh
+++ extrai-text.sh
@@ -8,8 +8,10 @@
 for ARQUIVO in *."pdf"
 do
    NOVO_NOME=$(basename "$ARQUIVO")
-   echo $NOVO_NOME   
+   echo "Iniciando corversão de documento " $NOVO_NOME " para formato TIFF"   
    convert -density 600  "$NOVO_NOME" -depth 8 -strip -background white -alpha off "$NOVO_NOME".tiff
-   echo $NOVO_NOME   
+   echo "Extração Textual de arquivo de IMAGEM TIFF " $NOVO_NOME " para formato TXT"   
    tesseract "$NOVO_NOME".tiff "$NOVO_NOME" -l por
+   echo "Removendo arquivo TIFF " "$NOVO_NOME".tiff 
+   rm "$NOVO_NOME".tiff   
 done
